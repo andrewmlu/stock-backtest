e6c4e129c88f30b98d0f6f6981602dcea8c31ad6
Andrew Lu <andrewmlu3@gmail.com>
2022-05-17 18:37:22 -0700

    minor update

--- server/apis/backtest.py
+++ server/apis/backtest.py
@@ -2,6 +2,7 @@ import requests
 from flask_restful import Api, Resource
 from dotenv import load_dotenv
 import os
+from datetime import datetime
 
 load_dotenv()
 
@@ -9,7 +10,25 @@ baseurl = 'https://sandbox.iexapis.com/stable'
 api_key = os.getenv('SANDBOX_KEY')
 
 class Backtest(Resource):
-	def get (self, ticker, startdate, enddate, freq, init_amnt=100, rec_amnt=0):
+
+	def calculate(self, series, init_amnt, rec_amnt):
+		historical_prices = [price['close'] for price in series]
+		holdings = [init_amnt/historical_prices[0]]
+		investments = [init_amnt]
+		for price in historical_prices[1:]:
+			if not rec_amnt == 0:
+				holdings.append(holdings[-1]+rec_amnt/price)
+				investments.append(investments[-1]+rec_amnt)
+			else:
+				holdings.append(holdings[0])
+				investments.append(investments[0])
+		for holding, investment, series_datapoint in zip(holdings, investments, series):
+			series_datapoint['value'] = series_datapoint['close'] * holding
+			series_datapoint['invested'] = investment
+			series_datapoint['holdings'] = holding
+		return series
+
+	def get (self, ticker, startdate, enddate, freq, init_amnt, rec_amnt):
 		
 		# check valid arguments
 		if int(enddate) < int(startdate) or int(freq) < 0:
@@ -35,7 +54,10 @@ class Backtest(Resource):
 			try:
 				response = requests.get(url)
 				response.raise_for_status()
-				res = response.json()
-				return res
+				res = response.json() # IEX provides chronologically most recent first
+				res.reverse()
+				for series_datapoint in res:
+					series_datapoint['date'] = datetime.utcfromtimestamp(series_datapoint['date']/1000).strftime('%Y-%m-%d')
+				return self.calculate(res, float(init_amnt), float(rec_amnt))
 			except requests.RequestException:
 				return None
